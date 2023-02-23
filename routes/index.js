5f968d4124c375ff32ec8dbd6d50fef43007e474
ozuaq <oza4456@gmail.com>
2023-02-23 15:35:14 +0900

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'ozayoshi\'s works' });
+});
+
+router.get('/site-description', function(req, res, next) {
+  res.render('site-description', { title: 'site-description' });
+});
+
+router.get('/ima-chat-top', function(req, res, next) {
+  res.render('ima-chat-top', { title: 'ima-chat-top' });
+});
+
+router.get('/ima-chat-description', function(req, res, next) {
+  res.render('ima-chat-description', { title: 'ima-chat-description' });
+});
+
+router.get('/research', function(req, res, next) {
+  res.render('research', { title: 'research' });
+});
+
+router.post('/ima-chat', function(req, res, next) {
+  let roomName = req.body.roomName;
+  let userName = req.body.userName;
+  console.log(roomName+userName);
+  res.render('ima-chat', { title: 'ima-chat', roomName: roomName, userName: userName });
 });
 
 module.exports = router;
